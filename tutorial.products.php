1859967445fbdf84c1a7223faafb80971ed8692d
tBrause <brause.torsten@gmail.com>
2022-06-03 23:06:06 +0200

    y

--- tutorial.products.php
+++ tutorial.products.php
@@ -6,56 +6,8 @@
 $collection = 'tutorial';
 $data = 'products';
 
-# Container
-$container = '';
+# Funktion getCollection laden
+include("assets/php/get-collection.php");
 
-# Fehler
-$error_msg = [];
-
-
-# ist die PHP-Erweiterung "mongodb" vorhanden
-if (extension_loaded("mongodb")) {
-
-    try {
-        # startet den Manager zum Aufbau einer Verbindung
-        $manager = new MongoDB\Driver\Manager($connection);
-
-        # Abfrage
-        $query = new MongoDB\Driver\Query([]);
-
-        # Auswahl der Collection und Daten / Dokumente
-        $cursor = $manager->executeQuery("" . $collection . '.' . $data . "", $query);
-
-        # Ergebnis
-        foreach ($cursor as $object) {
-            #var_dump($object);
-
-            $container .= $object->name . '<br>';
-            $container .= $object->price . '<br>';
-
-            if (isset($object->category)) {
-                $container .= $object->category . '<br>';
-            } else {
-                array_push($error_msg, $object->_id . ' (keine Kategorie)');
-            }
-            $container .= '<br><br>';
-        }
-    } catch (MongoConnectionExeption $e) {
-        var_dump($e);
-    }
-} else {
-    array_push($error_msg, 'PHP-MongoDB-Erweiterung installieren');
-}
-
-# Ausgabe der Fehlermeldungen
-if (count($error_msg) >= 1) {
-
-    echo 'Fehler : ' . count($error_msg) . '<br>';
-
-    foreach ($error_msg as $value) {
-        echo $value . '<br>';
-    }
-}
-
-# Ausgabe der Ergebnisse
-echo '<br>' . $container . '<br>';
+# Ergebnis
+getCollection($connection, $collection, $data);
